e32199f3fabdfd305174040f6d56eb48f5abd82d
Hugoleosilva <hugollsilva.dev@gmail.com>
2026-08-04 18:35:55 -0300

    Adiciona estrutura de componentes e serviços

---
+++ github-explorer/.next/dev/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"Vifr244uaZcEOvZWqCliGSj2v1mquDBN60xBTghaio0=\"\n}"
---
+++ github-explorer/.next/dev/static/chunks/github-explorer_219uq1s._.js
@@ -0,0 +1,5 @@
+(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: ["static/chunks/[root-of-the-server]__195o52e._.css","static/chunks/0ukt_next_dist_0na0lcv._.js","static/chunks/github-explorer_src_0y0rmt7._.css"],
+    source: "entry"
+});
