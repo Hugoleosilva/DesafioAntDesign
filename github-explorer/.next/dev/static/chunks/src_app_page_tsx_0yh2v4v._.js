2948a49201f9d930320c76cbbaff65cf5a39eea9
Hugoleosilva <hugollsilva.dev@gmail.com>
2026-08-04 20:13:14 -0300

    Remove .next do GitHub e adiciona ao gitignore

--- github-explorer/.next/dev/static/chunks/src_app_page_tsx_0yh2v4v._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/_0_jf1ki._.js","static/chunks/src_0ubvvgb._.css"],
-    source: "dynamic"
-});
