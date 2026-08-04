8df0fa3a01e5d8ae3bc28e33d9e38f01bd158208
Hugoleosilva <hugollsilva.dev@gmail.com>
2026-08-04 11:43:11 -0300

    Criação da Estrutura

---
+++ github-explorer/src/app/page.tsx
@@ -0,0 +1,10 @@
+import Header from "@/components/Header";
+import styles from "./page.module.scss";
+
+export default function Home() {
+  return (
+    <main className={styles.container}>
+      <Header />
+    </main>
+  );
+}
